5c9e5dd759ff319bfa4d10b0f867b4adff9c6e0c
Barbara <barbaralizama.proj@gmail.com>
2023-08-25 03:48:08 +0200

    r

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,6 @@ const app = require('../app');
 
 test('debería crear una instancia de la aplicación Express sin errores', () => {
   expect(() => {
-    app.listen(3000, () => {});
+    app.listen(5000, () => {});
   }).not.toThrow();
 });
